f9e7c5f2f06b2a876b6d5474134c03e2b1e6ae85
Muhammad Adib Bin Halizam <adibhali@umich.edu>
2023-11-11 21:05:47 -0500

    modify checklist page, add result page

--- main.js
+++ main.js
@@ -1,75 +1,132 @@
-document.addEventListener('DOMContentLoaded', function() {
-    let allCategory1 = document.querySelectorAll('input[type="checkbox"][value="category1"]');
-    let allCategory2 = document.querySelectorAll('input[type="checkbox"][value="category2"]');
-    let allCategory3 = document.querySelectorAll('input[type="checkbox"][value="category3"]');
-
-    let total1 = allCategory1.length;
-    let total2 = allCategory2.length;
-    let total3 = allCategory3.length;
+document.addEventListener('DOMContentLoaded', function () {
+    let totalCategory = [5, 3];
+    let totalSemanticElements = 0;
+    let totalTextAlternatives = 0;
+    const numCategory1 = 5;
+    const numCategory2 = 3;
 
     const button = document.getElementById('submit');
-
-    let resultSection = document.getElementById('finalResult');
-
-    let category1Applicable = true;
-    let category2Applicable = true;
-    let category3Applicable = true;
-
-    const radioApplicable1 = document.getElementById('applicable1');
-    const radioApplicable2 = document.getElementById('notApplicable1');
-    
-    radioApplicable1.addEventListener('click', function() {
-        for (let i = 0; i < allCategory1.length; i++) {
-            allCategory1[i].disabled = false;
-        }
-        category1Applicable = true;
-        total1 = allCategory1.length;
-        return;
-    });
-
-    radioApplicable2.addEventListener('click', function() {
-        for (let i = 0; i < allCategory1.length; i++) {
-            allCategory1[i].disabled = true;
-        }
-        category1Applicable = false;
-        total1 = 0;
-        return;
-    });
-
-    button.addEventListener('click', function() {
-        let check1 = 0;
-        let check2 = 0;
-        let check3 = 0;
-        let resultScore = 0;
-        if(category1Applicable == true) {
-            for (let i = 0; i < allCategory1.length; i++) {
-                if(allCategory1[i].checked) {
-                    check1++;
+    button.addEventListener('click', function () {
+        console.log('Button clicked');
+        const calculateTotalForCategory = (numQuestions, category) => {
+            let score = 0;
+            for (let question = 1; question <= numQuestions; question++) {
+                const selectedValue = document.querySelector(`input[name="category${category}_${question}"]:checked`).value;
+                if (selectedValue === "1") {
+                    score += 1;
+                } else if (selectedValue === "0") {
+                    // Do nothing for 0
+                } else if (selectedValue === "na") {
+                    totalCategory[category - 1] -= 1;
                 }
             }
-        }
+            return score;
+        };
 
-        if(category2Applicable == true) {
-            for (let i = 0; i < allCategory2.length; i++) {
-                if(allCategory2[i].checked) {
-                    check2++;
-                }
-            }
-        }
+        totalSemanticElements = calculateTotalForCategory(numCategory1, 1);
+        totalTextAlternatives = calculateTotalForCategory(numCategory2, 2);
 
-        if(category3Applicable == true) {
-            for (let i = 0; i < allCategory3.length; i++) {
-                if(allCategory3[i].checked) {
-                    check3++;
-                }
-            }
-        }
+        if (totalCategory[0] === 0)
+            totalSemanticElements = "Not Applicable";
+        else
+            totalSemanticElements = `${totalSemanticElements}/${totalCategory[0]}`;
+        if (totalCategory[1] === 0)
+            totalTextAlternatives = "Not Applicable";
+        else 
+            totalTextAlternatives = `${totalTextAlternatives}/${totalCategory[1]}`;
 
-        let fullScore = total1 + total2 + total3;
-        resultScore = check1 + check2 + check3;
-        let percentage = ((resultScore / fullScore) * 100).toFixed(2);
-        resultSection.innerHTML = "Accessibility Score : " + resultScore + "/" + fullScore +
-        "<br>" + "Accessibility Percentage : " + percentage + "%";
 
+        window.location.href = 'result.html';
+        localStorage.setItem('totalSemanticElements', totalSemanticElements);
+        localStorage.setItem('totalTextAlternatives', totalTextAlternatives);
+        // const resultSection = document.getElementById('finalResult');
+        // if (totalCategory[0] === 0)
+        //     totalSemanticElements = "Not Applicable";
+        // else
+        //     totalSemanticElements = `${totalSemanticElements}/${totalCategory[0]}`;
+        // if (totalCategory[1] === 0)
+        //     totalTextAlternatives = "Not Applicable";
+        // else 
+        //     totalTextAlternatives = `${totalTextAlternatives}/${totalCategory[1]}`;
+        // resultSection.innerHTML = "Accessibility Score : " + "<br>" +
+        //     "Semantic Element : " + totalSemanticElements + "<br>" +
+        //     "Text Alternative : " + totalTextAlternatives;
     });
 });
+
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     let allCategory1 = document.querySelectorAll('input[type="checkbox"][value="category1"]');
+//     let allCategory2 = document.querySelectorAll('input[type="checkbox"][value="category2"]');
+//     let allCategory3 = document.querySelectorAll('input[type="checkbox"][value="category3"]');
+
+//     let total1 = allCategory1.length;
+//     let total2 = allCategory2.length;
+//     let total3 = allCategory3.length;
+
+//     const button = document.getElementById('submit');
+
+//     let resultSection = document.getElementById('finalResult');
+
+//     let category1Applicable = true;
+//     let category2Applicable = true;
+//     let category3Applicable = true;
+
+//     const radioApplicable2_1 = document.getElementById('applicable2');
+//     const radioApplicable2_2 = document.getElementById('notApplicable2');
+    
+//     radioApplicable2_1.addEventListener('click', function() {
+//         for (let i = 0; i < allCategory2.length; i++) {
+//             allCategory2[i].disabled = false;
+//         }
+//         category2Applicable = true;
+//         total2 = allCategory1.length;
+//         return;
+//     });
+
+//     radioApplicable2_2.addEventListener('click', function() {
+//         for (let i = 0; i < allCategory2.length; i++) {
+//             allCategory2[i].disabled = true;
+//         }
+//         category2Applicable = false;
+//         total2 = 0;
+//         return;
+//     });
+
+//     button.addEventListener('click', function() {
+//         let check1 = 0;
+//         let check2 = 0;
+//         let check3 = 0;
+//         let resultScore = 0;
+//         if(category1Applicable == true) {
+//             for (let i = 0; i < allCategory1.length; i++) {
+//                 if(allCategory1[i].checked) {
+//                     check1++;
+//                 }
+//             }
+//         }
+
+//         if(category2Applicable == true) {
+//             for (let i = 0; i < allCategory2.length; i++) {
+//                 if(allCategory2[i].checked) {
+//                     check2++;
+//                 }
+//             }
+//         }
+
+//         if(category3Applicable == true) {
+//             for (let i = 0; i < allCategory3.length; i++) {
+//                 if(allCategory3[i].checked) {
+//                     check3++;
+//                 }
+//             }
+//         }
+
+//         let fullScore = total1 + total2 + total3;
+//         resultScore = check1 + check2 + check3;
+//         let percentage = ((resultScore / fullScore) * 100).toFixed(2);
+//         resultSection.innerHTML = "Accessibility Score : " + resultScore + "/" + fullScore +
+//         "<br>" + "Accessibility Percentage : " + percentage + "%";
+
+//     });
+// });
